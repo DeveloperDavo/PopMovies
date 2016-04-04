15f9a1723051cb13736a9a2ce5ada5821c39aaed
David Swallow <dswallow4@gmail.com>
2016-04-04 10:24:17 +0200

    Add popularMovies
    includes picasso
    has permission to access the internet
    loads an image in onCreate

---
+++ app/src/main/java/com/example/android/popularmoviesapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.popularmoviesapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView imageView = (ImageView) findViewById(R.id.photo_image_view);
+        Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(imageView);
+    }
+}
