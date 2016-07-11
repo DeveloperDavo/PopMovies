8e72af583ee4b95b153ae1c7ec19174f89dd7674
David Swallow <dswallow4@gmail.com>
2016-07-11 21:20:25 +0200

    Set up TestSuite

---
+++ app/src/androidTest/java/com/example/android/popularmoviesapp/data/TestDb.java
@@ -0,0 +1,7 @@
+package com.example.android.popularmoviesapp.data;
+
+/**
+ * Created by David on 10/07/16.
+ */
+public class TestDb {
+}
---
+++ app/src/androidTest/java/com/example/android/popularmoviesapp/utils/PollingCheck.java
@@ -0,0 +1,7 @@
+package com.example.android.popularmoviesapp.utils;
+
+/**
+ * Created by David on 10/07/16.
+ */
+public class PollingCheck {
+}
