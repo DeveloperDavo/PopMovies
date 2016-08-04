a8b6b4491746e0e4bd5308a0973345154eb204db
David Swallow <dswallow4@gmail.com>
2016-08-04 21:15:03 +0200

    Add MovieProvider

---
+++ app/src/androidTest/java/com/example/android/popularmoviesapp/data/TestProvider.java
@@ -0,0 +1,86 @@
+package com.example.android.popularmoviesapp.data;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.test.AndroidTestCase;
+
+/**
+ * Created by David on 13/07/16.
+ */
+public class TestProvider extends AndroidTestCase {
+
+    public static final String LOG_TAG = TestProvider.class.getSimpleName();
+
+    // Since we want each test to start with a clean slate, run deleteAllRecords
+    // in setUp (called by the test runner before each test).
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        deleteAllRecords();
+    }
+
+    /*
+       This helper function deletes all records from both database tables using the ContentProvider.
+       It also queries the ContentProvider to make sure that the database has been successfully
+       deleted, so it cannot be used until the Query and Delete functions have been written
+       in the ContentProvider.
+     */
+    public void deleteAllRecordsFromProvider() {
+        mContext.getContentResolver().delete(
+                MovieContract.MovieEntry.CONTENT_URI, // URI
+                null, // all rows
+                null // selection args
+        );
+
+        Cursor cursor = mContext.getContentResolver().query(
+                MovieContract.MovieEntry.CONTENT_URI, // URI
+                null, // all columns
+                null, // all rows
+                null, // selection args
+                null // sort order
+        );
+        assertEquals("Error: Records not deleted from movie table during delete", 0, cursor.getCount());
+        cursor.close();
+    }
+
+    /*
+      This helper function deletes all records from both database tables using the database
+      functions only.  This is designed to be used to reset the state of the database until the
+      delete functionality is available in the ContentProvider.
+    */
+    public void deleteAllRecordsFromDB() {
+        MovieDbHelper dbHelper = new MovieDbHelper(mContext);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        db.delete(MovieContract.MovieEntry.TABLE_NAME, null, null);
+        db.close();
+    }
+
+    /*
+        Student: Refactor this function to use the deleteAllRecordsFromProvider functionality once
+        you have implemented delete functionality there.
+     */
+    public void deleteAllRecords() {
+        deleteAllRecordsFromDB();
+    }
+
+    static private final int BULK_INSERT_RECORDS_TO_INSERT = 10;
+
+    static ContentValues[] createBulkInsertWeatherValues() {
+        ContentValues[] returnContentValues = new ContentValues[BULK_INSERT_RECORDS_TO_INSERT];
+
+        for (int i = 0; i < BULK_INSERT_RECORDS_TO_INSERT; i++) {
+            ContentValues movieValues = new ContentValues();
+            movieValues.put(MovieContract.MovieEntry.COLUMN_TITLE, "Title " + i);
+            movieValues.put(MovieContract.MovieEntry.COLUMN_POSTER_PATH, "Poster Path " + i);
+            movieValues.put(MovieContract.MovieEntry.COLUMN_OVERVIEW, "Overview " + i);
+            movieValues.put(MovieContract.MovieEntry.COLUMN_RATING, i);
+            movieValues.put(MovieContract.MovieEntry.COLUMN_RELEASE, "01-01-200" + i % 9);
+            movieValues.put(MovieContract.MovieEntry.COLUMN_FAVORITE, i % 2);
+            returnContentValues[i] = movieValues;
+        }
+        return returnContentValues;
+    }
+}
+
---
+++ app/src/main/java/com/example/android/popularmoviesapp/data/MovieProvider.java
@@ -0,0 +1,98 @@
+package com.example.android.popularmoviesapp.data;
+
+import android.annotation.TargetApi;
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by David on 13/07/16.
+ */
+public class MovieProvider extends ContentProvider {
+
+    private MovieDbHelper movieDbHelper;
+
+    @Override
+    public boolean onCreate() {
+        movieDbHelper = new MovieDbHelper(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        final Cursor cursor = movieDbHelper.getReadableDatabase().query(
+                MovieContract.MovieEntry.TABLE_NAME,
+                projection, selection, selectionArgs, null, null, sortOrder);
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        return MovieContract.MovieEntry.CONTENT_TYPE;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        final SQLiteDatabase db = movieDbHelper.getWritableDatabase();
+        Uri returnUri;
+
+        long _id = db.insert(MovieContract.MovieEntry.TABLE_NAME, null, values);
+        if (_id > 0)
+            returnUri = MovieContract.MovieEntry.buildMovieUri(_id);
+        else
+            throw new android.database.SQLException("Failed to insert row into " + uri);
+
+        getContext().getContentResolver().notifyChange(uri, null);
+        return returnUri;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int bulkInsert(Uri uri, ContentValues[] values) {
+        final SQLiteDatabase db = movieDbHelper.getWritableDatabase();
+        db.beginTransaction();
+        int returnCount = 0;
+        try {
+            for (ContentValues value : values) {
+                long _id = db.insert(MovieContract.MovieEntry.TABLE_NAME, null, value);
+                if (_id != -1) {
+                    returnCount++;
+                }
+            }
+            db.setTransactionSuccessful();
+        } finally {
+            db.endTransaction();
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return returnCount;
+    }
+
+    // You do not need to call this method. This is a method specifically to assist the testing
+    // framework in running smoothly. You can read more at:
+    // http://developer.android.com/reference/android/content/ContentProvider.html#shutdown()
+    @Override
+    @TargetApi(11)
+    public void shutdown() {
+        movieDbHelper.close();
+        super.shutdown();
+    }
+}
+
+
+
