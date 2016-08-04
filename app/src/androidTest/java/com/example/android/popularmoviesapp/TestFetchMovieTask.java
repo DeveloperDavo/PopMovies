0e276acecdbfbe3e8cf9a67109b2e232dff8bd0f
David Swallow <dswallow4@gmail.com>
2016-08-04 21:14:53 +0200

    Prepare Tests

---
+++ app/src/androidTest/java/com/example/android/popularmoviesapp/TestFetchMovieTask.java
@@ -0,0 +1,9 @@
+package com.example.android.popularmoviesapp;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Created by David on 04/08/16.
+ */
+public class TestFetchMovieTask extends AndroidTestCase {
+}
---
+++ app/src/androidTest/java/com/example/android/popularmoviesapp/data/TestMovieContract.java
@@ -0,0 +1,9 @@
+package com.example.android.popularmoviesapp.data;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Created by David on 04/08/16.
+ */
+public class TestMovieContract extends AndroidTestCase {
+}
